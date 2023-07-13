75d85b4f825c7f2b26b47178bb3880d3647a61be
Christy <christy.tomecek@yale.edu>
2023-07-13 11:07:58 -0400

    Create get_instances_by_parent-id.sql
    This SQL query was developed to find all top container and digital object instances attached to a parent archival object in ASpace. Version of ASpace is yale-rebased-v3.3.1 (so basically 3.3.1)

---
+++ get_instances_by_parent-id.sql
@@ -0,0 +1,15 @@
+# This SQL query was developed to find all top container and digital object instances attached to a parent archival object in ASpace. Version of ASpace is yale-rebased-v3.3.1 (so basically 3.3.1)
+SELECT resource.ead_id, resource.title AS collection, ao.title AS ao_title, ao.parent_id, ao.id AS item_id, 
+CONCAT(ao.id,',') as ingest_id, #ingest_id concat is because this is partially created for ingesting media and metadata into Aviary in bulk, which uses a CSV style form in the user interface.
+do.title AS do_title,
+tc.barcode, sc.indicator_2 
+FROM archival_object ao
+LEFT JOIN resource ON ao.root_record_id=resource.id
+LEFT JOIN instance ON ao.id=instance.archival_object_id
+LEFT JOIN instance_do_link_rlshp idlr ON instance.id=idlr.instance_id
+LEFT JOIN digital_object do ON idlr.digital_object_id=do.id
+LEFT JOIN sub_container sc ON instance.id=sc.instance_id
+LEFT JOIN top_container_link_rlshp tclr ON sc.id=tclr.sub_container_id
+LEFT JOIN top_container tc ON tclr.top_container_id=tc.id
+#WHERE ao.parent_id=
+#WHERE ao.parent_id IN ()
