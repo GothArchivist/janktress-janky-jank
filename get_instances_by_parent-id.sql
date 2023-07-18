a26a0b647d254e85f75dce32df856de8f5bfac01
Christy <christy.tomecek@yale.edu>
2023-07-18 09:04:41 -0400

    Update get_instances_by_parent-id.sql
    Changed EAD_ID to identifier and made it a substring so it exports better in CSV format.

--- get_instances_by_parent-id.sql
+++ get_instances_by_parent-id.sql
@@ -1,5 +1,5 @@
 # This SQL query was developed to find all top container and digital object instances attached to a parent archival object in ASpace. Version of ASpace is yale-rebased-v3.3.1 (so basically 3.3.1)
-SELECT resource.ead_id, resource.title AS collection, ao.title AS ao_title, ao.parent_id, ao.id AS item_id, 
+SELECT SUBSTRING(resource.identifier, 1,14), resource.title AS collection, ao.title AS ao_title, ao.parent_id, ao.id AS item_id, 
 CONCAT(ao.id,',') as ingest_id, #ingest_id concat is because this is partially created for ingesting media and metadata into Aviary in bulk, which uses a CSV style form in the user interface.
 do.title AS do_title,
 tc.barcode, sc.indicator_2 
